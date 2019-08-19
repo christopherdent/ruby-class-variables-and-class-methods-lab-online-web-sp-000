813b2fabcfe74113986a3a6a97e2d39dc86d507e
Christopher Dent <christopherdent01@gmail.com>
2019-08-19 01:03:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,39 @@
 class Song
   
 attr_accessor :name, :artist, :genre 
+    
+@@count = 0 
+
+@@artists = []
+
+@@genres = []
+
+  def initialize(name, artist, genre)
   
+    @@count += 1 
+  
+    @@genres << genre 
+  
+    @@artists << artist 
+  
+    name = name 
+    
+  end 
+
+
+
+
+def self.count 
+  @@count
+end 
+
+def genres 
+  @@genres 
+end 
+
+def self.artists 
+  @@artists 
+end 
+
 
 end 
