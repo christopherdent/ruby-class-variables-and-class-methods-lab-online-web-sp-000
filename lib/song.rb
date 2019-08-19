6f282cf2e99ddeeb4bfde5980295b4b8139fe3ee
Christopher Dent <christopherdent01@gmail.com>
2019-08-19 01:30:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ def self.genre_count
     
     binding.pry 
     
-    @@genre_count["value"] = number
+    @@genre_count["value"] = value.count
  end 
     
     # To add a key to the person hash:
